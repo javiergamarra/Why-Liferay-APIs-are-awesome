13ccc0b902bca4f1e5e741fd3d47e71ce6c0737a
Javier Gamarra <nhpatt@gmail.com>
2020-09-24 18:01:03 +0200

    9. Use Languages

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default () => {
     endpoint.searchParams.append('nestedFields', 'contentValue');
     endpoint.searchParams.append("filter", "keywords/any(k:k eq 'tiktok')");
 
-    fetch(endpoint, {headers: {"Authorization": "Basic dGVzdDFAdGVzdDEuY29tOnRlc3Qx"}})
+    fetch(endpoint, {headers: {"Authorization": "Basic dGVzdDFAdGVzdDEuY29tOnRlc3Qx", "X-Accept-All-Languages": true}})
       .then(response => response.json())
       .then(data => {
         console.log(data)
@@ -22,7 +22,7 @@ export default () => {
         setVideos(data.items.map(({content: document}) => ({
           creator: document.creator,
           remove: document.actions.delete,
-          song: document.title,
+          song: document.title_i18n ? document.title_i18n[window.navigator.language] : document.title,
           description: document.description,
           url: document.contentValue ? ('data:video/mp4;base64,' +  document.contentValue) : 'http://localhost:8080/' + document.contentFields[0].contentFieldValue.document.contentUrl
         })));
