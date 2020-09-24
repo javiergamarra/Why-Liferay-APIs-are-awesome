9f1de7d871f2a2eccda9697536e5b0cda05e8c2a
Javier Gamarra <nhpatt@gmail.com>
2020-09-24 17:31:16 +0200

    3. Using GraphQL

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import {gql, useQuery} from '@apollo/client';
+import {useParams} from 'react-router-dom';
+
+const userProfileQuery = gql`query documents($filter: String!){
+  documents(siteKey: "Guest", filter: $filter) {
+    items {
+      creator {
+        id
+        name
+      }
+      contentUrl
+      title
+    }
+  }
+}`
 
 export default () => {
 
+  const {creatorId} = useParams();
+
+  const {loading, data} = useQuery(userProfileQuery, {
+    variables: {
+      filter: `creatorId eq ${creatorId}`
+    }
+  });
+
+  console.log(data)
+
   return (
-    <div>Hola!</div>
+    <div className="video">{
+      data && data.documents && data.documents.items.map(document => (
+        <video style={{width:"33%"}}
+          src={'http://localhost:8080/' + document.contentUrl}
+        />
+      ))
+    }</div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/o/graphql',
+  cache: new InMemoryCache(),
+  headers: {"Authorization": "Basic dGVzdEBsaWZlcmF5LmNvbTp0ZXN0"}
+});
+
 ReactDOM.render(
-  <App/>,
+  <ApolloProvider client={client}><App/></ApolloProvider>,
   document.getElementById('root'),
 );
