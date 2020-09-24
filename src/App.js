b69f680fb609316f60b118863e2668d8df274760
Javier Gamarra <nhpatt@gmail.com>
2020-09-24 17:13:08 +0200

    2 - Use REST APIs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Video from './Video';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch,} from "react-router-dom";
 import UserProfile from './UserProfile';
 
 export default () => {
-  const [videos] = useState(FAKE_VIDEOS);
+  const [videos, setVideos] = useState(FAKE_VIDEOS);
+
+  useEffect(() => {
+
+    fetch('http://localhost:8080/o/headless-delivery/v1.0/sites/Guest/documents', {headers: {"Authorization": "Basic dGVzdEBsaWZlcmF5LmNvbTp0ZXN0"}})
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+
+        setVideos(data.items.map(document => ({
+          creator: document.creator,
+          song: document.title,
+          description: document.description,
+          url: 'http://localhost:8080/' + document.contentUrl
+        })));
+      })
+  }, [])
 
   return (
     <Router>
